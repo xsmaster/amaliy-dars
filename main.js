2a027b0c288c97f1010974c74c8f3535268ff1cd
xsmaster <165388332+xsmaster@users.noreply.github.com>
2024-06-07 16:24:42 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,112 @@
+// 1) Ikkita o'zgaruvchi yarating va ularning ichiga son saqlang va bir birini cosole.logda qo'shib chiqaring chiqaring
+
+
+
+
+
+// let num1 = 78;
+// let num2 = 12;
+// console.log(num1 + num2);
+
+
+
+
+// 2) let a  = '12' shu 12 sonini data typeni o'zgartirib number qilib chiqaring 
+// bering. Yani bu hozir string typeda siz number tyga o'zgartiring
+
+
+
+
+
+// let num3 = ('74'(Number))
+// console.log(num3);
+
+
+
+
+
+// 3) randomli son yasang va shu randomli sonning juft yoki toq ekanligini aniqlab bering
+
+
+
+
+
+// let randomNumber = Math.round(Math.random() * 100);
+
+// if(randomNumber % 2 == 0){
+//     console.log(randomNumber +'juft son');
+// }else{
+//     console.log(randomNumber +'toq son');
+// }
+
+
+
+
+// 4) promptdan bitta so'z kiriting agar o'sha so'z Mars so'ziga teng bolsa o'sha so'zning
+//  o'zi chiqsin agar boshqa so'z bo'lsa Mars It school deb chiqsin
+
+
+
+
+
+
+// function salat() {
+//     let a = "Mars"
+//     if(Userworld = a ) {
+//         alert("Mars")
+//     }else{
+//         alert("It school")
+//     }
+// }
+   
+
+
+
+
+
+
+// 5) bitta array yarating va uning ichiga 5 ta so'z kiriting. Keyin For yordamida o'sha arraydagi 
+// so'zlar ichidan uzunligi 5 va undan kattalarini bohsqa bir arrayga qo'shib chiqaring
+
+
+
+
+
+
+// let salat = ["Olma" ,"Apelsin" ,"Mandarin" ,"Kivi" ,"Banan"]
+// let box1 = []
+// let box2 = []
+// for (let index = 0; index < salat.length; index++) {
+//     if(salat[index].length > 5){
+//         box1.push(salat[index])
+//     }else{
+//         box2.push(salat[index])
+//     }
+    
+// }
+// console.log(salat);
+// console.log(box1);
+// console.log(box2);
+
+
+
+
+
+// 6) for orqali birdan 100gacha bolgan sonlarni juft yoki toqqa ajratib chiqaring
+
+
+
+
+
+// let box1 = []
+// let box2 = []
+// for (let index = 0; index < 100; index++) {
+//     if(randomNumber % 2 == 0){
+//      box1.push(idex);;
+// }else{
+//      box2.push(idex);
+//  }
+// }
+
+// console.log(box1);
+// console.log(box2);
